49270b0c06c251a29dc5bf08594e80a06f5d03a3
Md Selim <whiteblueskyss@gmail.com>
2025-11-23 21:58:03 +0600

    Added first code in go

---
+++ 01starting/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	fmt.Println("Hello, World!", a)
+
+	ok := false
+	ok = true
+	if ok==true {
+		fmt.Println("You are pretty!")
+	}
+
+	var val int
+	fmt.Println("Value is:", val)
+}
