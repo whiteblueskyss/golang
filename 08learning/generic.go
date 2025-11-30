254ee25d4d0c6bd95197a727ffb4a8b36d001d76
Md Selim <whiteblueskyss@gmail.com>
2025-11-30 10:13:47 +0600

    Added some more basic code.

---
+++ 08learning/generic.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func printSlice[T comparable, V string](items []T, name V) { // T any,  T interface{} (same to any) , T string | int | bool we can also use like this.
+
+	for _, item := range items {
+		fmt.Println(item, name)
+	}
+}
+
+// func printStringSlice(items []string) {
+// 	for _, item := range items {
+// 		fmt.Println(item)
+// 	}
+// }
+
+//
+// LIFO
+// type stack[T any] struct {
+// 	elements []T
+// }
+
+func generic() {
+	// myStack := stack[string]{
+	// 	elements: []string{"golang"},
+	// }
+
+	// fmt.Println(myStack)
+
+	// nums := []int{1, 2, 3}
+	// names := []string{"golang", "typescript"}
+	vals := []bool{true, false, true}
+	// printStringSlice(names)
+	printSlice(vals, "john")
+}
---
+++ 08learning/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// interfaceDemo()
+
+	p := &Person{Name: "Selim"}
+
+	SayHello(p)
+}
+
+type Greeter interface {
+	Greet()
+}
+
+type Person struct {
+	Name string
+}
+
+// TODO: Add the method here so that *Person satisfies Greeter
+func (p *Person) Greet() {
+	fmt.Println("Hello, my name is", p.Name)
+}
+func SayHello(g Greeter) {
+	g.Greet()
+}
